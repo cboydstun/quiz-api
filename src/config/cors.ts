8335fd793402a8b78fe38565216577bf67d1288d
Chris Boydstun <chrisboydstun@gmail.com>
2024-10-07 12:40:07 -0500

    cors options

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,8 @@
 // config/cors.ts
 
 export const corsOptions = {
-  origin: "*",
-  credentials: true,
+  origin: "https://www.dronepilotquiz.com", // Allow only your frontend domain
+  credentials: true,                        // Allow cookies and credentials
+  methods: ['GET', 'POST', 'OPTIONS'],      // Specify allowed methods
+  allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'], // Specify allowed headers
 };
