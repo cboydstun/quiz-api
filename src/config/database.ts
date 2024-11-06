2d3d049e97e6f9fe6a94dbd56315a4359a1f47c0
Chris Boydstun <chrisboydstun@gmail.com>
2024-11-06 15:32:43 -0600

    longer timeout on DB connect

--- src/config/database.ts
+++ src/config/database.ts
@@ -16,9 +16,9 @@ export const connectDB = async (): Promise<void> => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI as string, {
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 30000,
       maxPoolSize: 10, // Adjust based on your needs
-    });    
+    });
     logger.info("Connected to MongoDB");
   } catch (error) {
     logger.error("Failed to connect to MongoDB", { error });
